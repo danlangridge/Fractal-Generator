ff768e08c99748b0129c3fd8df89f1b3ee7cf4df
dan <dlangridge@gmail.com>
2014-01-07 23:27:22 -0800

    Retrieve and Display input to and from user

--- FractalGenerator.hs
+++ FractalGenerator.hs
@@ -1,10 +1,12 @@
+module Main where
 
-main = do
-  let a = 1
-  let b = 2
-  let i = (-1)^(1/2)
-  let c = 10
-  let starting_z = a + b*i 
-  let next_z = 1
-  let next_z = starting_z^2 + c
-  print True
+main = do putStrLn "Please enter initial a value" 
+          a_value <- getLine
+          putStrLn "Please enter initial b value"
+          b_value <- getLine
+          putStrLn "Please enter c value"
+          c_value <- getLine
+          putStrLn ("Here is the equation: Z_1 = " ++ a_value ++ "^2 + 2(" ++ b_value ++ "*" ++ a_value ++ ")i - " ++ b_value ++ "^2 + " ++ c_value);
+          --let new_a = a_value*a_value - b_value*b_value + c_value
+          --let new_b = 2*a_value*b_value
+          --putStrLn ("New Z value: " ++ new_a ++ " + " ++ new_b ++ "i")
