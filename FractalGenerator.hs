71bca215d95cfc146aad15ffcedb04d0c6fdb3cd
dan <dlangridge@gmail.com>
2014-01-08 20:55:29 -0800

    Added new z calculation code

--- FractalGenerator.hs
+++ FractalGenerator.hs
@@ -1,5 +1,8 @@
 module Main where
 
+randomNumber number = do 
+                         return 32
+
 main = do putStrLn "Please enter initial a value" 
           a_value <- getLine
           putStrLn "Please enter initial b value"
@@ -7,6 +10,7 @@ main = do putStrLn "Please enter initial a value"
           putStrLn "Please enter c value"
           c_value <- getLine
           putStrLn ("Here is the equation: Z_1 = " ++ a_value ++ "^2 + 2(" ++ b_value ++ "*" ++ a_value ++ ")i - " ++ b_value ++ "^2 + " ++ c_value);
-          --let new_a = a_value*a_value - b_value*b_value + c_value
-          --let new_b = 2*a_value*b_value
-          --putStrLn ("New Z value: " ++ new_a ++ " + " ++ new_b ++ "i")
+          let new_a = a_value*a_value - b_value*b_value + c_value
+          let new_b = 2*a_value*b_value
+          putStrLn ("New Z value: " ++ new_a ++ " + " ++ new_b ++ "i")
+          
