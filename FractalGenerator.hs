abaca8bd5c878133dced129f4c365385283b3fa8
dan <dlangridge@gmail.com>
2014-01-13 23:31:46 -0800

    Added readvalue function

--- FractalGenerator.hs
+++ FractalGenerator.hs
@@ -3,12 +3,14 @@ module Main where
 randomNumber number = do 
                          return 32
 
-main = do putStrLn "Please enter initial a value" 
-          a_value <- getLine
-          putStrLn "Please enter initial b value"
-          b_value <- getLine
-          putStrLn "Please enter c value"
-          c_value <- getLine
+
+readValue x = do putStrLn "Please enter " ++ x ++ " value: "
+                 return getLine
+
+
+main = do a_value <- readValue 'a' 
+          b_value <- readValue 'b'
+          c_value <- readValue 'c'
           putStrLn ("Here is the equation: Z_1 = " ++ a_value ++ "^2 + 2(" ++ b_value ++ "*" ++ a_value ++ ")i - " ++ b_value ++ "^2 + " ++ c_value);
           let new_a = a_value*a_value - b_value*b_value + c_value
           let new_b = 2*a_value*b_value
