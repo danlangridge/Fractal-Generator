4c6d958510f493ad8c2b784d2c5902390a7a4665
dan <dlangridge@gmail.com>
2014-01-14 23:03:19 -0800

    Haskell isnt playing ball

--- FractalGenerator.hs
+++ FractalGenerator.hs
@@ -1,18 +1,10 @@
 module Main where
 
-randomNumber number = do 
-                         return 32
 
+getInt = do a <- readLn
+            let b = a
+            return (read b :: Int)
 
-readValue x = do putStrLn "Please enter " ++ x ++ " value: "
-                 return getLine
-
-
-main = do a_value <- readValue 'a' 
-          b_value <- readValue 'b'
-          c_value <- readValue 'c'
-          putStrLn ("Here is the equation: Z_1 = " ++ a_value ++ "^2 + 2(" ++ b_value ++ "*" ++ a_value ++ ")i - " ++ b_value ++ "^2 + " ++ c_value);
-          let new_a = a_value*a_value - b_value*b_value + c_value
-          let new_b = 2*a_value*b_value
-          putStrLn ("New Z value: " ++ new_a ++ " + " ++ new_b ++ "i")
-          
+main = do let a_value = getInt           
+          let y = [x | x <-[1..10], mod x 2 == 0, x^x > x*x]
+          print a_value 
