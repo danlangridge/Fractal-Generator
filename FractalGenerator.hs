cc79ac49d27d89c0dac265605b553157378d2db4
dan <dlangridge@gmail.com>
2014-01-15 22:13:21 -0800

    Added pixel resolution function

--- FractalGenerator.hs
+++ FractalGenerator.hs
@@ -5,6 +5,27 @@ getInt = do a <- readLn
             let b = a
             return (read b :: Int)
 
-main = do let a_value = getInt           
-          let y = [x | x <-[1..10], mod x 2 == 0, x^x > x*x]
-          print a_value 
+realZ :: float -> float -> float
+realZ a b = a^2 - 1*b^2 + 2 
+
+imaginaryZ :: float -> float -> float
+imaginaryZ a b = 2*a*b
+
+makeBMP x y x_c y_c = do  
+  if x == x_c && y == y_c
+    return
+  if x == x_c 
+     then
+       let z_a = realZ x y 
+       let z_b = imaginaryZ x y
+       makeBMP x y x_c+1 y_c
+  if y == y_c
+     then
+       let z_a = realZ x y 
+       let z_b = imaginaryZ x y
+       makeBMP x y 0 y_c+1 
+
+makeBMPInit x y = makeBMP x y 0 0
+
+main =
+  makeBMPInit 100 100
